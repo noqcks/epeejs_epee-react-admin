8c191f52a065b8ae5725573f4e3079266c43c57c
noqcks <benji@093b.org>
2024-04-04 19:04:09 +0000

    Upgrade dependencies

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -32,7 +32,7 @@ const PageHeader: React.FC<PageHeaderProps> = () => {
                   window.location.href = '/login';
                 }}
               >
-                <PoweroffOutlined style={{ marginRight: 15 }} />
+                <span style={{ marginRight: 15 }}><PoweroffOutlined /></span>
                 <span>退出登录</span>
               </span>
             </Menu.Item>
