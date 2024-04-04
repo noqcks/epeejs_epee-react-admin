052d8f182e70c50c5164886fe96b3303af058364
Benji Visser <benji@093b.org>
2024-04-04 03:21:45 +0000

    Upgrade dependencies

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -32,7 +32,7 @@ const PageHeader: React.FC<PageHeaderProps> = () => {
                   window.location.href = '/login';
                 }}
               >
-                <PoweroffOutlined />
+                <PoweroffOutlined style={{}} />
                 <span>退出登录</span>
               </span>
             </Menu.Item>
