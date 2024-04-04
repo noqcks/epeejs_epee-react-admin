74d58356dfe7414bdf2ae30795e5dff91ac7b313
noqcks <benji@093b.org>
2024-04-04 19:07:25 +0000

    Upgrade dependencies

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,6 +29,7 @@ const Login: React.FC<LRouteComponentProps> = ({ history }) => {
           <Input
             prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
           />
+        </Form.Item>
         <Form.Item>
           <Form.Item name="remember" valuePropName="checked" noStyle>
             <Checkbox>Remember me</Checkbox>
